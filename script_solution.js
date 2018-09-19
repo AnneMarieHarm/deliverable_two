99bfc572cda1156357823a2b0612e333cd2e36a9
AnneMarieHarm <amharm@earthlink.net>
2018-09-19 18:36:39 -0400

    first commit JavaScript

---
+++ script_solution.js
@@ -0,0 +1,53 @@
+//Exercise_One
+
+var num = prompt("Enter A Number!"); 
+
+var output = 0;
+
+for(var i = 1; i <= num; i++) {
+  output = output + i;
+}
+  console.log(output);	
+  
+//Exercise_Two  
+
+var wantPlay = prompt("Do you want to play?");
+var string = "";
+var word;
+
+if (wantPlay == "yes") {
+
+  do {
+    word = prompt ("Enter a Word.");
+    string = string + word + " ";
+    wantPlay = prompt("Do you want to play again?");
+  } while 
+    (wantPlay == "yes")
+}
+console.log(string);
+
+//Exercise_Three -- work in progress
+
+var result = prompt("Would you like to print your name?");
+
+var string;
+
+string = prompt("What is your name?");
+
+while (response == "yes"); {
+console.log();
+}
+
+//Exercise_Four
+var TimeOday = prompt("What time of day is it?");
+
+if (TimeOday == 'morning') { 
+  console.log("Since it is morning, you should be eating breakfast. We suggest eggs and toast.")
+} else if (TimeOday == 'noon') {
+  console.log("Since it is noon, you should be eating lunch. We suggest a salad.")
+} else if (TimeOday == 'evening') {
+  console.log("Since it is evening, you should be eating dinner. We suggest chicken and rice.")
+} else {
+console.log("We don't understand; please tell us again.")
+}
+
---
+++ script_solution_2.js
@@ -0,0 +1,10 @@
+var greeting = "Enter A Number!";
+
+var num = prompt(greeting);
+
+var total = 0;
+
+for (var i = 1; i <= num; i++) {
+  total = total + i;
+}
+console.log (total);
